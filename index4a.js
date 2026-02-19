6a5966dc61e13542d4bc91c80b41c86fd44e186a
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-19 13:55:51 +0530

    updated

--- index4a.js
+++ index4a.js
@@ -4,9 +4,21 @@ app.listen(8080, () => {
   console.log("Server started");
 });
 
-app.get("/:n1/:n2", (req, res) => {
-  let num1 = parseInt(req.params.n1);
-  let num2 = parseInt(req.params.n2);
+app.get("/:x/:y", (req, res) => {
+  let num1 = parseInt(req.params.x);
+  let num2 = parseInt(req.params.y);
   let result = num1 + num2;
   res.send(result);
+});
+
+app.get("/x/:x/y/:y/z/:z", (req, res) => {
+  let num1 = parseInt(req.params.x);
+  let num2 = parseInt(req.params.y);
+  let num3 = parseInt(req.params.z);
+  let result = num1 + num2 + num3;
+  res.send(result);
+});
+
+app.get("/:a/:b/:c/:d", (req, res) => {
+  res.send("Hello Student");
 });

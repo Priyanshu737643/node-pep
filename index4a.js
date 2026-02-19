cd641d1ad8a0b6fc7d6f5b6fea62a58d0330a4ee
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-19 13:43:09 +0530

    updated

--- index4a.js
+++ index4a.js
@@ -4,9 +4,9 @@ app.listen(8080, () => {
   console.log("Server started");
 });
 
-app.get("/:id/:email", (req, res) => {
-  let num1 = parseInt(req.params.id);
-  let num2 = parseInt(req.params.email);
+app.get("/:n1/:n2", (req, res) => {
+  let num1 = parseInt(req.params.n1);
+  let num2 = parseInt(req.params.n2);
   let result = num1 + num2;
   res.send(result);
 });

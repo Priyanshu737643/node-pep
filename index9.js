646684cf46544c18591ccd5ad7c297f736e58769
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-20 11:55:17 +0530

    update

--- index9.js
+++ index9.js
@@ -1,5 +1,26 @@
-import express from 'express';
+//! Middleware
+
+import express from "express";
 const app = express();
 app.listen(8080, () => {
-    console.log("server running");
-})
+  console.log("server running");
+});
+
+const logger = (req, res, next) => {
+  req.message = "This is logger function";
+  //   console.log(req.url);
+  next();
+};
+
+// app.use(logger);
+
+app.get("/", logger,(req, res) => {
+//   app.use(logger);
+  console.log(req.message);
+  res.json(req.url);
+});
+
+app.get("/home", (req, res) => {
+    console.log(req.message);
+  res.json(req.url);
+});

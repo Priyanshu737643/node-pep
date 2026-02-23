9ecf324761114b1e2444206eae4e2c2e531d1bd1
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-23 10:15:54 +0530

    updated

--- index12.js
+++ index12.js
@@ -4,15 +4,28 @@ import express from "express";
 const app = express();
 app.listen(8080, () => console.log("Server running"));
 
-const Router = express.Router();
+const userRouter = express.Router();
+const productRouter = express.Router();
 
 //? localhost:8080/api/users/
-http: Router.get("/", (req, res) => {
-  res.send("Hello World");
+
+// userRouter
+userRouter.get("/", (req, res) => {
+  res.send("Get request of User Router");
 });
 
-Router.post("/", (req, res) => {
-  res.send("This is post request");
+userRouter.post("/", (req, res) => {
+  res.send("This is post request of user router");
 });
 
-app.use("/api/users", Router);
+// productRouter
+productRouter.get("/", (req, res) => {
+  res.send("Get request of Product Router");
+});
+
+productRouter.post("/", (req, res) => {
+  res.send("This is post request of product router");
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
